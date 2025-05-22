73822c1383451363485c4d517b43d386212b8e96
Samarth-170904 <samarthmule1704@gmail.com>
2025-05-22 19:55:09 +0530

    Delete music lib file

--- Projects/LunaAsistant/musiclib.py
+++
@@ -1,5 +0,0 @@
-music = {
-        "happy": "https://www.youtube.com/watch?v=ZbZSe6N_BXs",  # Example: "Happy" by Pharrell Williams
-        "sweet home": "https://www.youtube.com/watch?v=ye5BuYf8q4o",  # Example: "Sweet Home Alabama"
-        "music": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # Example: Rickroll (generic "music")
-}
